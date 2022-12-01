3ff869f99a6b49c3900107c602dd43d5035e50cd
Kyu <rocketword@gmail.com>
2022-12-01 15:20:14 -0600

    Initial Commit: Day 1

---
+++ day1/day1.py
@@ -0,0 +1,28 @@
+with open("input.txt", 'r') as f:
+    input_text = f.readlines()
+
+current_calories = 0
+
+top_calories = []
+
+for c in input_text:
+    this_calorie = c[:-1]
+    if not this_calorie:
+        if len(top_calories) < 3:
+            top_calories.append(current_calories)
+        else:
+            if current_calories > top_calories[0]:
+                top_calories.insert(0, current_calories)
+            elif current_calories > top_calories[1]:
+                top_calories.insert(1, current_calories)
+            elif current_calories > top_calories[2]:
+                top_calories.insert(2, current_calories)
+
+        if len(top_calories) > 3:
+            top_calories = top_calories[:3]
+        current_calories = 0
+    else:
+        this_calorie = int(this_calorie)
+        current_calories += this_calorie
+
+print(f"Top calories: {top_calories}\nSum = {sum(top_calories)}")
