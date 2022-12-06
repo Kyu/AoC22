cbf03ac2cfc2ae3ffabcb4629b8cd3108cf24196
Kyu <rocketword@gmail.com>
2022-12-06 13:52:22 -0600

    Day5: Added larger inputs from GoT forum, optimized parsing and processing of data

--- day5/day5.py
+++ day5/day5.py
@@ -1,11 +1,14 @@
 from string import ascii_uppercase
+import time
+
+import copy
 
 
-with open("input.txt", 'r') as f:
+with open("aoc_2022_day05_large_input_GoT_6mb.txt   ", 'r') as f:
     input_text = f.readlines()
 
 
-crates = dict()
+crates = {}
 
 def get_crate_data(line: str):
     for i in range(len(line)):
@@ -16,19 +19,24 @@ def get_crate_data(line: str):
 
                 this_crate = crates.get(crate_number, [])
 
-                this_crate.insert(0, item)
+                this_crate.append(item)
                 crates[crate_number] = this_crate
 
 
+print("Starting crate parsing")
+now = time.time()
 for start in input_text:
     if '[' in start:
         get_crate_data(start)
     else:
         break
 
+for crt in crates.values():
+    crt.reverse()
 
+print(f"Done! Took {time.time() - now}")
 def create_crate_string(cr: dict):
-    crate_string = ""
+    crate_string = ''
     for k,v in sorted(cr.items()):
         print(f"{k}: {v[-1]}")
         crate_string += v[-1]
@@ -45,41 +53,46 @@ def get_command_data(command: str):
 
 
 def part1():
-    p1_crates = crates.copy()
+    p1_crates = copy.deepcopy(crates)
 
     for text in input_text:
         if 'move' in text:
             command = text[5:]
-
             number, origin, destination = get_command_data(command)
 
-            while number:
-                number -= 1
+            objects_1 = p1_crates[origin]
 
-                obj = p1_crates.get(origin, []).pop()
-                p1_crates.get(destination, []).append(obj)
+            to_move_1 = objects_1[-number:]
+            to_move_1.reverse()
+
+            del p1_crates[origin][-number:]
+            p1_crates[destination].extend(to_move_1)
 
     return p1_crates
 
-
-
 def part2():
-    p2_crates = crates.copy()
+    p2_crates = copy.deepcopy(crates)
 
     for text in input_text:
         if 'move' in text:
             command = text[5:]
             number, origin, destination = get_command_data(command)
 
-            objects = p2_crates.get(origin, [])
+            objects_2 = p2_crates[origin]
 
-            to_move = objects[-number:]
-            p2_crates[origin] = objects[:-number]
-
-            p2_crates.get(destination, []).extend(to_move)
+            to_move_2 = objects_2[-number:]
+            del p2_crates[origin][-number:]
+            p2_crates[destination].extend(to_move_2)
 
     return p2_crates
 
 if __name__ == "__main__":
+    print("Starting: ")
+
+    now = time.time()
     print(f"Part 1: {create_crate_string(part1())}")
+    print(f"Done! Took {time.time() - now}")
+
+    now = time.time()
     print(f"Part 2: {create_crate_string(part2())}")
+    print(f"Done! Took {time.time() - now}")
